bb45d30f0a60e79d1c7170d0084f02e2dc2fe7ac
Navishth Rao <navishthrao06@gmail.com>
2026-03-28 19:52:26 +0530

    Done with implementations

--- Parallel Merge Sort/src/app/ParallelMergeSort/parallelmergeSort.cpp
+++ Parallel Merge Sort/src/app/ParallelMergeSort/parallelmergeSort.cpp
@@ -1,7 +1,71 @@
 #include "parallelmergeSort.hpp"
-
+#include<thread>
+#include<iostream>
+#include<algorithm>
 ParallelMergeSort::ParallelMergeSort(std::vector<int> *nums){
     this->nums=nums;
 }
 
 ParallelMergeSort::~ParallelMergeSort(){};
+
+void ParallelMergeSort::recursiveSort(int left,int right){
+
+        const int THRESHOLD = 5000;
+
+          if(left>=right){
+            return;
+        }
+        
+        if (right - left < THRESHOLD) {
+        std::sort(nums->begin() + left, nums->begin() + right + 1);
+        return;
+    }
+    
+
+      
+        int mid=left+(right-left)/2;       //we avoid (left + right)/2 due to integer overflow.
+        std::thread thread_1([this, left, mid] { this->recursiveSort(left, mid); });
+        std::thread thread_2([this, mid, right] { this->recursiveSort(mid + 1, right); });
+
+        thread_1.join();
+        thread_2.join();
+
+         std::vector<int> result;
+        int i=left;
+        int j=mid+1;
+
+          while (i <= mid && j <= right) {
+        if ((*nums)[i] <= (*nums)[j]) {
+            result.push_back((*nums)[i]);
+            i++;
+        } else {
+            result.push_back((*nums)[j]);
+            j++;
+        }
+    }
+
+    while (i <= mid) {
+        result.push_back((*nums)[i]);
+        i++;
+    }
+
+    while (j <= right) {
+        result.push_back((*nums)[j]);
+        j++;
+    }
+
+    for (int k = 0; k < result.size(); k++) {
+        (*nums)[left + k] = result[k];
+    }
+
+}
+
+    void ParallelMergeSort::sort(){
+         if ((*nums).size() == 0) {
+        exit(1);
+    }
+
+    std::thread thread_1([this] { this->recursiveSort(0, (*nums).size() - 1); });
+    thread_1.join();
+    }
+
--- Parallel Merge Sort/src/app/main.cpp
+++ Parallel Merge Sort/src/app/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include "mergeSort.hpp"
+#include "parallelmergeSort.hpp"
 #include<chrono>
 int main(int argc, char *argv[]){
 
     const int SIZE=1000;
     std::vector<int> nums(SIZE);
+     std::vector<int> nums1(SIZE);
     for (int i = 0; i < SIZE; ++i)
     {
         nums[i]=rand()%1000;
+        nums1[i]=rand()%1000;
 
     }
     MergeSort* mergesort = new MergeSort(&nums);
@@ -18,4 +21,13 @@ int main(int argc, char *argv[]){
     std::cout<<"MergeSort Algorithm time taken: "<<mergesortDuration.count()<<" seconds"<<std::endl;
 
     delete mergesort;
+
+    ParallelMergeSort* mergesort1 = new ParallelMergeSort(&nums1);
+    start = std::chrono::high_resolution_clock::now();
+    mergesort1->sort();
+    end = std::chrono::high_resolution_clock::now();
+    mergesortDuration = end-start;
+    std::cout<<"Parallel MergeSort Algorithm time taken: "<<mergesortDuration.count()<<" seconds"<<std::endl;
+
+    delete mergesort1;
 }
